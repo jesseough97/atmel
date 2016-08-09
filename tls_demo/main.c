c5225e9b661b0c407d802082aff72c0e7f7faf5a
David Garske <david@wolfssl.com>
2016-08-09 09:22:42 -0700

    Added support for switching the console UART (either the EDBG_CDC UART or EXT at PTB10/PTB11).

--- tls_demo/main.c
+++ tls_demo/main.c
@@ -56,6 +56,26 @@
 /** UART module for debug. */
 static struct usart_module cdc_uart_module;
 
+#if 1
+/* Use EXT3 UART */
+#define CONF_STDIO_USART_MODULE  EXT3_UART_MODULE
+#define CONF_STDIO_MUX_SETTING   EXT3_UART_SERCOM_MUX_SETTING
+#define CONF_STDIO_PINMUX_PAD0   EXT3_UART_SERCOM_PINMUX_PAD0
+#define CONF_STDIO_PINMUX_PAD1   EXT3_UART_SERCOM_PINMUX_PAD1
+#define CONF_STDIO_PINMUX_PAD2   EXT3_UART_SERCOM_PINMUX_PAD2
+#define CONF_STDIO_PINMUX_PAD3   EXT3_UART_SERCOM_PINMUX_PAD3
+#define CONF_STDIO_BAUDRATE      115200
+#else
+/* Use EDBG UART */
+#define CONF_STDIO_USART_MODULE  EDBG_CDC_MODULE
+#define CONF_STDIO_MUX_SETTING   EDBG_CDC_SERCOM_MUX_SETTING
+#define CONF_STDIO_PINMUX_PAD0   EDBG_CDC_SERCOM_PINMUX_PAD0
+#define CONF_STDIO_PINMUX_PAD1   EDBG_CDC_SERCOM_PINMUX_PAD1
+#define CONF_STDIO_PINMUX_PAD2   EDBG_CDC_SERCOM_PINMUX_PAD2
+#define CONF_STDIO_PINMUX_PAD3   EDBG_CDC_SERCOM_PINMUX_PAD3
+#define CONF_STDIO_BAUDRATE      115200
+#endif
+
 /**
  * \brief Configure UART console.
  */
@@ -64,14 +84,14 @@ static void configure_console(void)
 	struct usart_config usart_conf;
 
 	usart_get_config_defaults(&usart_conf);
-	usart_conf.mux_setting = EDBG_CDC_SERCOM_MUX_SETTING;
-	usart_conf.pinmux_pad0 = EDBG_CDC_SERCOM_PINMUX_PAD0;
-	usart_conf.pinmux_pad1 = EDBG_CDC_SERCOM_PINMUX_PAD1;
-	usart_conf.pinmux_pad2 = EDBG_CDC_SERCOM_PINMUX_PAD2;
-	usart_conf.pinmux_pad3 = EDBG_CDC_SERCOM_PINMUX_PAD3;
-	usart_conf.baudrate    = 115200;
+	usart_conf.mux_setting = CONF_STDIO_MUX_SETTING;
+	usart_conf.pinmux_pad0 = CONF_STDIO_PINMUX_PAD0;
+	usart_conf.pinmux_pad1 = CONF_STDIO_PINMUX_PAD1;
+	usart_conf.pinmux_pad2 = CONF_STDIO_PINMUX_PAD2;
+	usart_conf.pinmux_pad3 = CONF_STDIO_PINMUX_PAD3;
+	usart_conf.baudrate    = CONF_STDIO_BAUDRATE;
 
-	stdio_serial_init(&cdc_uart_module, EDBG_CDC_MODULE, &usart_conf);
+	stdio_serial_init(&cdc_uart_module, CONF_STDIO_USART_MODULE, &usart_conf);
 	usart_enable(&cdc_uart_module);
 }
 
